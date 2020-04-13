d3e58bd0a777b34007f81cbd32206ad68b98a631
CamiloSte <61854474+CamiloSte@users.noreply.github.com>
2020-04-13 15:24:36 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Libro.Data;
+using Libro.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Libro
+{
+    public partial class App : Application
+    {
+
+       public static TokenBaseController tokenController;
+       public static UsuarioBaseController usuarioController;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        public UsuarioBaseController UsuarioController
+        {
+            get
+            {
+                if (usuarioController==null)
+                {
+                    usuarioController = new UsuarioBaseController();
+                }
+                return usuarioController;
+            }
+        }
+        public TokenBaseController TokenController
+        {
+            get
+            {
+                if (tokenController == null)
+                {
+                    tokenController = new TokenBaseController();
+                }
+                return tokenController;
+            }
+        }
+    }
+}
