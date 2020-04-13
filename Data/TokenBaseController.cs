f5eb097fe53f241c98e436e06f581e080ddeb7e3
CamiloSte <61854474+CamiloSte@users.noreply.github.com>
2020-04-13 15:13:47 -0500

    Add files via upload

---
+++ Data/TokenBaseController.cs
@@ -0,0 +1,59 @@
+﻿using Libro.Modelo;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Libro.Data
+{
+    public class TokenBaseController
+    {
+        static object locker = new object();
+        SQLiteConnection database;
+
+        public TokenBaseController()
+        {
+            database = DependencyService.Get<ISQLite>().GetConnection();
+            database.CreateTable<Token>();
+        }
+
+        public Token GetToken()
+        {
+            lock (locker)
+            {
+                if (database.Table<Token>().Count() == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return database.Table<Token>().First();
+                }
+            }
+        }
+        public int guardarToken(Token token)
+        {
+            lock (locker)
+            {
+                if (token.Id != 0)
+                {
+                    database.Update(token);
+                    return token.Id;
+                }
+                else
+                {
+                    return database.Insert(token);
+                }
+            }
+        }
+        public int eliminarToken(int Id)
+        {
+            lock (locker)
+            {
+                return database.Delete<Token>(Id);
+            }
+        }
+    
+}
+}
---
+++ Data/UsuarioBaseController.cs
@@ -0,0 +1,58 @@
+﻿using Libro.Modelo;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Libro.Data
+{
+    public class UsuarioBaseController
+    {
+        static object locker = new object();
+        SQLiteConnection database;
+        
+        public UsuarioBaseController()
+        {
+            database = DependencyService.Get<ISQLite>().GetConnection();
+            database.CreateTable<Usuario>();
+        }
+
+        public Usuario GetUsuario()
+        {
+            lock (locker)
+            {
+                if (database.Table<Usuario>().Count() == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return database.Table<Usuario>().First();
+                }
+            }
+        }
+        public int guardarUsuariio(Usuario usuario)
+        {
+            lock (locker)
+            {
+                if (usuario.Correo !="")
+                {
+                    database.Update(usuario);
+                    return usuario.Id;
+                }
+                else
+                {
+                    return database.Insert(usuario.Correo,usuario.Contraseña);
+                }
+            }
+        }
+        public int eliminarUsuario(int Id)
+        {
+            lock (locker)
+            {
+                return database.Delete<Usuario>(Id);
+            }
+        }
+    }
+}
